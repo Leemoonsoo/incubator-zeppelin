8f9225ed45d2c386bdb833a27fbe5efd6e9f2681
Lee moon soo <moon@apache.org>
2019-04-14 03:38:37 -0700

    Make InterpreterContext create rest api endpoint running in Interpreter process

--- zeppelin-interpreter/src/main/java/org/apache/zeppelin/interpreter/remote/RemoteInterpreterServer.java
+++ zeppelin-interpreter/src/main/java/org/apache/zeppelin/interpreter/remote/RemoteInterpreterServer.java
@@ -8,7 +8,7 @@
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by app[licable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -17,6 +17,7 @@
 
 package org.apache.zeppelin.interpreter.remote;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.apache.commons.lang.exception.ExceptionUtils;
@@ -71,6 +72,7 @@ import org.apache.zeppelin.resource.Resource;
 import org.apache.zeppelin.resource.ResourcePool;
 import org.apache.zeppelin.resource.ResourceSet;
 import org.apache.zeppelin.resource.WellKnownResourceName;
+import org.apache.zeppelin.serving.RestApiServer;
 import org.apache.zeppelin.scheduler.Job;
 import org.apache.zeppelin.scheduler.Job.Status;
 import org.apache.zeppelin.scheduler.JobListener;
@@ -140,6 +142,8 @@ public class RemoteInterpreterServer extends Thread
   // cluster manager client
   ClusterManagerClient clusterManagerClient = ClusterManagerClient.getInstance();
   ZeppelinConfiguration zconf = ZeppelinConfiguration.create();
+
+  private RestApiServer restApiServer = RestApiServer.singleton();
 
   public RemoteInterpreterServer(String intpEventServerHost,
                                  int intpEventServerPort,
@@ -388,7 +392,7 @@ public class RemoteInterpreterServer extends Thread
     }
   }
 
-  protected InterpreterGroup getInterpreterGroup() {
+  public InterpreterGroup getInterpreterGroup() {
     return interpreterGroup;
   }
 
@@ -398,6 +402,11 @@ public class RemoteInterpreterServer extends Thread
 
   protected RemoteInterpreterEventClient getIntpEventClient() {
     return intpEventClient;
+  }
+
+  @VisibleForTesting
+  public void setIntpEventClient(RemoteInterpreterEventClient intpEventClient) {
+    this.intpEventClient = intpEventClient;
   }
 
   private void setSystemProperty(Properties properties) {
@@ -788,6 +797,7 @@ public class RemoteInterpreterServer extends Thread
         .setInterpreterOut(output)
         .setIntpEventClient(intpEventClient)
         .setProgressMap(progressMap)
+        .setRestApiServer(restApiServer)
         .build();
   }
 
